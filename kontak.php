a58be81bd0ffe394dd7bdea3aa53e66ce99ee7b0
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-04-28 01:38:44 +0700

    update: navbar & locations

--- kontak.php
+++ kontak.php
@@ -22,8 +22,9 @@
       </a>
       <ul class="nav-links">
         <li><a href="index.php">Produk</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="kontak.php" class="active">Contact</a></li>
+        <li><a href="about.php">Tentang Kami</a></li>
+        <li><a href="galeri.php">Galeri</a></li>
+        <li><a href="kontak.php" class="active">Kontak</a></li>
       </ul>
       <div class="cart">
         <a href="cart.php">
@@ -140,10 +141,15 @@
       <div class="location-finder">
         <h2>Temukan Kami</h2>
         <p class="intro-text">
-          Produk kami tersebar di beberapa supermarket seperti Hypermart Kemang, Kawaraci, dan sebagainya.
+          Produk kami tersebar di beberapa supermarket seperti Hypermart Kemang, Karawaci, dan sebagainya.
         </p>
 
         <div class="location-maps">
+          <div class="location-card">
+            <h3> Supermall Karawaci</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4116.048028512499!2d106.60444837499034!3d-6.226811593761304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fd6b81f00491%3A0xc5927a7be82e23b8!2sSupermal%20Karawaci!5e1!3m2!1sid!2sid!4v1745773781088!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+
           <div class="location-card">
             <h3>Hypermart Kemang Village</h3>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4115.777121946206!2d106.8102797749907!3d-6.2612789937273226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1097407a2c3%3A0x228bb72cc4f619d6!2sHypermart%20-%20Kemang%20Village!5e1!3m2!1sid!2sid!4v1745444843677!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -160,18 +166,37 @@
           </div>
 
           <div class="location-card">
-            <h3>Hypermart Taman Yasmin</h3>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4113.381599966561!2d106.76893817499324!3d-6.558213793434922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c586972e82af%3A0x40e0ef72245e5206!2sHypermart%20-%20Taman%20Yasmin!5e1!3m2!1sid!2sid!4v1745445161448!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <h3>Hypermart Ciputra Mall</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4114.807883891208!2d106.9234071749917!3d-6.383077093607306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69955ffa31f9c1%3A0x446263558a2ebbe7!2sHypermart%20-%20Mall%20Ciputra%20Cibubur!5e1!3m2!1sid!2sid!4v1745773937541!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
 
           <div class="location-card">
-            <h3>Hyfresh Rempoa</h3>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4115.600371662609!2d106.75822477499086!3d-6.283665393705216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f13074cc0f9d%3A0x8478b5aee25ede9d!2sHyfresh%20-%20Supermarket%20Rempoa!5e1!3m2!1sid!2sid!4v1745445229892!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <h3>Hypermart Pluit Village</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4116.903364088388!2d106.78862107498959!3d-6.116717893869886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1d7589d72cd7%3A0xb999762791a1bfd1!2sHypermart%20Pluit%20Village!5e1!3m2!1sid!2sid!4v1745773977331!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+
+          <div class="location-card">
+            <h3>Cilandak Town Square</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4115.5389081662315!2d106.7966428749909!3d-6.291431493697583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1e677b91d2f%3A0x110a936a8d153b27!2sPrimo%20Supermarket%20Citos!5e1!3m2!1sid!2sid!4v1745774075621!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+
+          <div class="location-card">
+            <h3>Hypermart Puri Indah</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4116.371073261582!2d106.73693827499018!3d-6.185460593802076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f773ffffffd9%3A0xc1af7598c5e3c7fc!2sHypermart%20-%20Puri%20Indah!5e1!3m2!1sid!2sid!4v1745774151569!5m2!1sid!2sid" w allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+
+          <div class="location-card">
+            <h3>Hypermart Gading Serpong</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4115.912909965584!2d106.62676837499053!3d-6.24402639374432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fc2555555569%3A0x8ad6c8a7abb3516c!2sHypermart%20Gading%20Serpong!5e1!3m2!1sid!2sid!4v1745774185357!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
 
           <div class="location-card">
             <h3>Happy Harvest Summarecon Bandung</h3>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4110.012579450077!2d107.69608897499683!3d-6.954486693045836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c3b518e5b3f9%3A0x212b94425606a2f6!2sHappy%20Harvest%20Summarecon%20Mall%20Bandung!5e1!3m2!1sid!2sid!4v1745445269820!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div class="location-card">
+            <h3>Happy Harvest Summarecon Bogor</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4112.87208083372!2d106.83591957499381!3d-6.6196603933745095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c7e971d0cb43%3A0x5178a757e47058d7!2sHappy%20Harvest%20Summarecon%20Bogor!5e1!3m2!1sid!2sid!4v1745774239028!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </div>
@@ -180,7 +205,7 @@
 
   <!-- footer start -->
   <footer>
-    &copy; 2024 - Afiat Barokah Mandri | All rights reserved. Designed By
+    &copy; 2024 - Afiat Barokah Mandiri | All rights reserved. Designed By
     <a
       style="text-decoration: underline; color: #fff"
       href="https://webwirausahamuda.com"
